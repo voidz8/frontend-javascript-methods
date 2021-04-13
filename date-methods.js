1d825f314e4ae707717e6f97971cdb260a389505
voidz8 <renzovermue@hotmail.com>
2021-04-13 13:57:50 +0200

    finished date-methods.js

--- date-methods.js
+++ date-methods.js
@@ -13,8 +13,11 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
-
+const today = new Date();
+console.log(today.getHours() + ":" + today.getMinutes() );
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+console.log(today.toDateString())
